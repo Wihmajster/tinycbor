99350eb49070ffe528add4983469f3d66d21acd7
Gabriel Brzezinski <gabriel@gabrielb.dev>
2026-02-12 16:39:12 +0000

    platformio: add native build manifest and generated public headers

---
+++ src/tinycbor.h
@@ -0,0 +1,14 @@
+/* Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef TINYCBOR_H
+#define TINYCBOR_H
+
+/*
+ * Compatibility umbrella header used by Arduino/PlatformIO integrations.
+ * Upstream TinyCBOR's public C API lives in cbor.h and cborjson.h.
+ */
+#include "cbor.h"      // IWYU pragma: export
+#include "cborjson.h"  // IWYU pragma: export
+
+#endif /* TINYCBOR_H */
